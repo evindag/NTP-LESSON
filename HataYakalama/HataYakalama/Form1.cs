3a0f4e816d52660bc867414f5d344c2be8a2ee84
Evin <80323753+evindag@users.noreply.github.com>
2024-12-17 15:56:24 +0300

    Add files via upload

---
+++ HataYakalama/HataYakalama/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HataYakalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi, kare;
+            try
+            {
+                sayi = Convert.ToInt32(textBox1.Text);
+                kare = sayi * sayi;
+                MessageBox.Show(kare.ToString());
+            }
+            catch 
+            {
+                MessageBox.Show("Hatalı giriş yaptınız. Lütfen sadece sayısal bir değer giriniz."); 
+            }
+            
+        }
+    }
+}
---
+++ KosulluForDongusu/KosulluForDongusu/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KosulluForDongusu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int baslangic, bitis;
+
+            baslangic = Convert.ToInt32(textBox1.Text);
+            bitis = Convert.ToInt32(textBox2.Text);
+
+            for (int i = baslangic; i < bitis; i++)
+            {
+                if (i%3==0)
+                {
+                    listBox1.Items.Add(i);
+                }
+                if (i%5==0)
+                {
+                    listBox2.Items.Add(i);
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    listBox3.Items.Add(i);
+                }
+            }
+        } 
+    }
+}
