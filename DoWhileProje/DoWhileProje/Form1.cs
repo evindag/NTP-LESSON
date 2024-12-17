b3580ecb6c1aa72d7b902ec68e1cbe1bb1e17b92
Evin <80323753+evindag@users.noreply.github.com>
2024-12-17 15:52:41 +0300

    Add files via upload

---
+++ DoWhileProje/DoWhileProje/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoWhileProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int baslangic = Convert.ToInt32(textBox1.Text);
+            int bitis = Convert.ToInt32(textBox2.Text);
+
+            int i = baslangic;
+
+            do
+            {
+                if (i%3==0 && i%5==0)
+                {
+                    listBox1.Items.Add(i);
+                }
+                if (i % 7 == 0)
+                {
+                    listBox2.Items.Add(i);
+                }
+                if (i % 3 == 0 && i % 5 == 0 && i%7==0)
+                {
+                    listBox3.Items.Add(i);
+                }
+                i++;
+            } while (i<=bitis);
+        }
+    }
+}
