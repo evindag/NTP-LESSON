37fb52d46ea23a14ef81795226d6e16240415cd4
Evin <80323753+evindag@users.noreply.github.com>
2024-12-17 15:59:16 +0300

    Add files via upload

---
+++ Switch_Case_Haftanin_Gunleri_2/Switch_Case_Haftanin_Gunleri_2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_Case_Haftanin_Gunleri_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int gun = Convert.ToInt16(textBox1.Text);
+
+            switch (gun)
+            {
+                case 1:             
+                case 2:        
+                case 3:  
+                case 4:    
+                case 5:
+                    MessageBox.Show("Haftaiçi");
+                    break;
+                case 6:   
+                case 7:
+                    MessageBox.Show("Haftasonu");
+                    break;
+                default:
+                    MessageBox.Show("Bir Hata Oluştu.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Switch_case_gecti_kaldi_uygulamasi/Switch_case_gecti_kaldi_uygulamasi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_case_gecti_kaldi_uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (textBox1.Text)
+            {
+                case "0":
+                case "1":
+                    label3.Text = "Kaldı";
+                    break;
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                    label3.Text = "geçti";
+                    break;
+                default:
+                    MessageBox.Show("Lütfen 0-5 arasında bir sayı giriniz!");
+                    break;
+            }
+        }
+    }
+}
